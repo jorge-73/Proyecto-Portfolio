714532e94642ba8fdcdb3e8f266cb6efbfcd97ee
jorge-73 <jorge_ap_73@hotmail.com>
2022-10-19 20:06:02 -0300

    Update

--- admin/header.php
+++ admin/header.php
@@ -23,6 +23,9 @@
             <li class="nav-item">
                 <a class="nav-link" href="message.php">Message |</a>
             </li>
+            <li class="nav-item">
+                <a class="nav-link" href="cerrar.php">Exit |</a>
+            </li>
         </ul>
         </div>
     </div>
